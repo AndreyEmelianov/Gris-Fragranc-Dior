1148db18f50f27e1efd21db544da53cfe6be3edb
Andrey Emelianov <621077@mail.ru>
2024-03-21 20:27:54 +0300

    реализовал разметку второй секции about, добавил изображения, и стилизовал согласно макету

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-const swiper = new Swiper('.swiper', {
+const topSlider = new Swiper('.top__slider', {
   effect: 'fade',
 
   autoplay: {
     delay: 4000,
     disableOnInteraction: false,
   },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const aboutSlider = new Swiper('.about__slider', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
 
   navigation: {
     nextEl: '.swiper-button-next',
